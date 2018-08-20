140b918ee1dd467d55c7fbd8b50fce8a745cec27
immanuelv <immanuel.valle@gmail.com>
2018-08-20 10:43:56 +0800

    Added register validations and endpoint

--- app/imports/api/users/endpoints.js
+++ app/imports/api/users/endpoints.js
@@ -1,15 +1,64 @@
-import Projects from "../projects/projects";
-import Videos from "../videos/videos";
+import validator from 'validator';
+
+import Projects from '../projects/projects';
+import Videos from '../videos/videos';
 
 export default function(API) {
-  // API.addRoute(
-  //   'login',
-  //   { authRequired: false },
-  //   {
-  //     post: function() {
-  //       console.log('called', this.request)
-  //     },
-  //   }
-  // );
+  API.addRoute(
+    'register',
+    { authRequired: false },
+    {
+      post: function() {
+        // Get params
+        const { email, password, username } = this.request.body;
 
+        // Check null fields
+        if (!email | !password || !username) {
+          return {
+            status: 'error',
+            message: 'Please fill out all fields',
+          };
+        }
+
+        // Check invalid email format
+        if (!validator.isEmail(email)) {
+          return {
+            status: 'error',
+            message: `Wrong e-mail format for ${email}`,
+          };
+        }
+
+        // Check for existing username
+        const isExisting = Meteor.users.findOne({ username });
+
+        if (isExisting) {
+          return {
+            status: 'error',
+            message: `Username already exists`,
+          };
+        }
+
+        try {
+          const user = Accounts.createUser({
+            email,
+            password,
+            username,
+            profile: {},
+          });
+
+          return {
+            status: 'success',
+            userId: user,
+            email,
+            username,
+          };
+        } catch (err) {
+          return {
+            status: 'error',
+            message: err.reason,
+          };
+        }
+      },
+    }
+  );
 }
