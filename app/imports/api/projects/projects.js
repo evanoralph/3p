c783c6b6036e82927ef14d502ffddffac7055f0d
immanuelv <immanuel.valle@gmail.com>
2018-10-03 18:47:19 +0800

    Added hook to remove associated videos with the deleted project

--- app/imports/api/projects/projects.js
+++ app/imports/api/projects/projects.js
@@ -1,2 +1,14 @@
+import Videos from '../videos/videos';
 const Projects = new Mongo.Collection('projects');
+
+// Delete associated videos when a project is deleted
+Projects.after.remove((userId, doc) => {
+
+  // Get project id
+  const projectId = doc._id;
+  // Remove videos
+  Videos.remove({ projectId });
+
+})
+
 export default Projects;
